baf9353cb76d0b0795a30f8ab11563419e205231
Pavel Kirilin <win10@list.ru>
2020-12-23 10:20:59 +0400

    Rewritten with new aopi_index_builder version.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- aopi_python/main.py
+++ aopi_python/main.py
@@ -1,6 +1,6 @@
 from aopi_index_builder import PackageIndex
 
-from aopi_python.ctx import context, plugin_prefix
+from aopi_python.ctx import context
 from aopi_python.models import models_list
 from aopi_python.routes import main_router
 
@@ -8,7 +8,6 @@ from aopi_python.routes import main_router
 def main() -> PackageIndex:
     print(context)
     return PackageIndex(
-        prefix=plugin_prefix,
         router=main_router,
         models=models_list,
         help="To use it add <aopi-url>/python/simple as your index-url",
