14499d180890a9e891e1193f55c7f07deefe3ea4
Pavel Kirilin <win10@list.ru>
2021-01-07 11:24:32 +0400

    Updated python index.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- aopi_python/main.py
+++ aopi_python/main.py
@@ -1,6 +1,11 @@
 from aopi_index_builder import PackageIndex
 
 from aopi_python.ctx import context
+from aopi_python.index_functions import (
+    find_packages_func,
+    get_package_info_func,
+    get_versions_func,
+)
 from aopi_python.models import models_list
 from aopi_python.roles import RolesEnum
 from aopi_python.routes import main_router
@@ -13,5 +18,9 @@ def main() -> PackageIndex:
         target_language="python",
         db_models=models_list,
         roles=RolesEnum,
-        help=f"To use it add <aopi-url>{context.prefix}/simple as your index-url",
+        plugin_help=f"To use it add {{aopi-url}}/"
+        f"{context.prefix}/simple as your index-url",
+        find_packages_func=find_packages_func,
+        get_package_info_func=get_package_info_func,
+        get_versions_func=get_versions_func,
     )
